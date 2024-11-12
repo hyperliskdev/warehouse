daa9bfdd7ebb552299ea3869f560b0791d257c72
Carter Tomlenovich <cartertomlenovich@gmail.com>
2024-11-12 11:51:40 -0500

    using S3 for model storage

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,20 @@
-use aws_config::meta::region::RegionProviderChain;
-use aws_config::BehaviorVersion;
-use aws_sdk_dynamodb::{Client, Error};
+use aws_config::{meta::region::RegionProviderChain, SdkConfig};
+use warp::{Error, Filter};
 
+pub async fn get_shared_config() -> SdkConfig {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-2");
+    let region = region_provider.region().await.unwrap();
+    let shared_config = aws_config::from_env().region(region).load().await;
 
-// pub async fn upload_object(
-//     client: &aws_sdk_s3::Client,
-//     bucket_name: &str,
-//     file_name: &str,
-//     key: &str,
-// ) -> Result<aws_sdk_s3::operation::put_object::PutObjectOutput, S3ExampleError> {
-//     let body = aws_sdk_s3::primitives::ByteStream::from_path(std::path::Path::new(file_name)).await;
-//     client
-//         .put_object()
-//         .bucket(bucket_name)
-//         .key(key)
-//         .body(body.unwrap())
-//         .send()
-//         .await
-//         .map_err(S3ExampleError::from)
-// }
+    return shared_config;
+}
 
-/// Lists your DynamoDB tables in the default Region or us-east-1 if a default Region isn't set.
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
-    
-    let config = aws_config::defaults(BehaviorVersion::latest())
-        .region(region_provider)
-        .load()
+
+    warp::serve(warp::get().map(|| warp::reply::html("Hello, World!")))
+        .run(([127, 0, 0, 1], 3030))
         .await;
-
-    let client = Client::new(&config);
-
-    
-
-
-
 
     Ok(())
 }
